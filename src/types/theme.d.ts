c36b639fd4227d59211fc04a2f6007c5c2023f12
frontEnd-Jair <jair_correa@outlook.com.br>
2025-11-03 12:49:00 -0300

    Feat: Create Themes

---
+++ src/types/theme.d.ts
@@ -0,0 +1,45 @@
+import type { Stringifier } from 'styled-components/dist/types'
+
+export interface Theme {
+  appBackground: string
+  appColor: string
+  appDefaultStroke: string
+  appLogo: string
+  appSkeletonFrom: string
+  appSkeletonTo: string
+  buttons: {
+    alert: string
+    alertColor: string
+    alertHover: string
+    disabled: string
+    disabledColor: string
+    primary: string
+    primaryColor: string
+    primaryHover: string
+  }
+  card: {
+    alert: string
+    background: string
+    border: string
+    success: string
+    warning: string
+  }
+  textInput: {
+    active: string
+    activeColor: string
+    borderColor: string
+    disabled: string
+    disabledBorderColor: string
+    disabledColor: string
+    placeholderColor: string
+  }
+  typographies: {
+    error: string
+    subtitle: string
+    success: string
+  }
+}
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends Theme {}
+}
