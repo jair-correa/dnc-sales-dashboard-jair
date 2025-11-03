f2d935e87d57a19bfdadb5837e70bcc0aba68109
frontEnd-Jair <jair_correa@outlook.com.br>
2025-11-02 22:10:23 -0300

    fix(app) Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<>LOGIN</>}></Route>
-      <Route path="/cadastro" element={<>CADASTRO</>}></Route>
-      <Route path="/home" element={<>HOME</>}></Route>
-      <Route path="/leads" element={<>LEADS</>}></Route>
-      <Route path="/perfil" element={<>PERFIL</>}></Route>
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<>LOGIN</>}></Route>
+        <Route path="/cadastro" element={<>CADASTRO</>}></Route>
+        <Route path="/home" element={<>HOME</>}></Route>
+        <Route path="/leads" element={<>LEADS</>}></Route>
+        <Route path="/perfil" element={<>PERFIL</>}></Route>
+      </Routes>
+    </Router>
   )
 }
 
